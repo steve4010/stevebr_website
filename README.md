a2028c33066d4c0bb49e09c217c30d5298774c41
Steve <steveoreno517@gmail.com>
2026-07-20 20:31:23 -0400

    Edit ReadMe file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # stevebr_website
-My website..
+
+My Hugo website.
+
+When I push changes to main a Github action builds the hugo site and pushes the public files to my shared host.
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USER=lvregyly
-HOST=68.65.122.195
-DIR=/home/lvregyly/stevebr.com   # the directory where your website files should go
-
-rm -rf public && hugo --minify && rsync -avz -e 'ssh -p 21098' --delete --stats --progress public/ ${USER}@${HOST}:${DIR} # this will delete everything on the server that's not in the local public folder 
-
-exit 0
